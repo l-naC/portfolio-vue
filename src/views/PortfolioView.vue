e9529de68b6f207dfab9c3feaddd480601e1c169
Léna <lena.clavier@gmail.com>
2021-01-21 17:57:04 +0100

    Add Gsap scrollTrigger animation responsive

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -35,48 +35,111 @@ export default {
   methods: {
     startAnimations: function () {
   
-      // let tl_about = gsap.timeline({
-      //   // yes, we can add it to an entire timeline!
-      //   scrollTrigger: {
-      //     trigger: "#section-about",
-      //     toggleActions: "restart pause resume pause",
-      //     start: "center bottom",
-      //     end: "center top"
-      //   }
-      // })
+      ScrollTrigger.matchMedia({
+	
+        // desktop
+        "(min-width: 800px)": function() {
+          // setup animations and ScrollTriggers for screens 800px wide or greater (desktop) here...
+          // These ScrollTriggers will be reverted/killed when the media query doesn't match anymore.
 
-      // // add animations and labels to the timeline
-      // tl_about.from('#image-about', {x: 200, opacity: 0, duration: 1.5})
-      //   .from('#text-about', {x: -300, opacity: 0, duration: 1}, "-=1")
+          let tl_about = gsap.timeline({
+            // yes, we can add it to an entire timeline!
+            scrollTrigger: {
+              trigger: "#section-about",
+              toggleActions: "restart pause resume pause",
+              start: "center bottom",
+              end: "center top"
+            }
+          })
 
-      // let tl_skills = gsap.timeline({
-      //   // yes, we can add it to an entire timeline!
-      //   scrollTrigger: {
-      //     trigger: "#section-skills",
-      //     toggleActions: "restart pause resume pause",
-      //     start: "center bottom",
-      //     end: "center top"
-      //   }
-      // });
+          // add animations and labels to the timeline
+          tl_about.from('#image-about', {x: 200, opacity: 0, duration: 1.5})
+            .from('#text-about', {x: -300, opacity: 0, duration: 1}, "-=1")
 
-      // // add animations and labels to the timeline
-      // tl_skills.from('.title-skills', {x: -300, y: -200, opacity: 0, duration: 1})
-      //   .from('.card-skills', {x: -300, rotation: 360, duration: 1}, "-=1")
-      //   .from('#infos-skills', {x: 300, y: 300, opacity: 0, duration: 1}, "-=1")
+          let tl_skills = gsap.timeline({
+            // yes, we can add it to an entire timeline!
+            scrollTrigger: {
+              trigger: "#section-skills",
+              toggleActions: "restart pause resume pause",
+              start: "center bottom",
+              end: "center top"
+            }
+          });
 
-      // let tl_contact = gsap.timeline({
-      //   // yes, we can add it to an entire timeline!
-      //   scrollTrigger: {
-      //     trigger: "#section-contact",
-      //     toggleActions: "restart pause resume pause",
-      //     start: "center bottom",
-      //     end: "center top"
-      //   }
-      // })
+          // add animations and labels to the timeline
+          tl_skills.from('.title-skills', {x: -300, y: -200, opacity: 0, duration: 1})
+            .from('.card-skills', {x: -300, rotation: 360, duration: 1}, "-=1")
+            .from('#infos-skills', {x: 300, y: 300, opacity: 0, duration: 1}, "-=1")
 
-      // // add animations and labels to the timeline
-      // tl_contact.from('#image-contact', {x: -200, y: 500, opacity: 0, duration: 1.5})
-      //   .from('#form-contact', {x:200, y: 500, opacity: 0, duration: 1}, "-=1")
+          let tl_contact = gsap.timeline({
+            // yes, we can add it to an entire timeline!
+            scrollTrigger: {
+              trigger: "#section-contact",
+              toggleActions: "restart pause resume pause",
+              start: "center bottom",
+              end: "center top"
+            }
+          })
+
+          // add animations and labels to the timeline
+          tl_contact.from('#image-contact', {x: -200, y: 500, opacity: 0, duration: 1.5})
+            .from('#form-contact', {x:200, y: 500, opacity: 0, duration: 1}, "-=1")
+        },
+
+        // mobile
+        "(max-width: 799px)": function() {
+          let tl_about = gsap.timeline({
+            // yes, we can add it to an entire timeline!
+            scrollTrigger: {
+              trigger: "#section-about",
+              toggleActions: "restart pause resume pause",
+              start: "center bottom",
+              end: "center top"
+            }
+          })
+
+          // add animations and labels to the timeline
+          tl_about.from('#text-about', {x: -300, opacity: 0, duration: 1}, "-=1")
+          .from('#image-about', {x: -400, opacity: 0, duration: 1.5})
+            
+          // The ScrollTriggers created inside these functions are segregated and get
+          // reverted/killed when the media query doesn't match anymore.
+          let tl_skills = gsap.timeline({
+            // yes, we can add it to an entire timeline!
+            scrollTrigger: {
+              trigger: "#section-skills",
+              toggleActions: "restart pause resume pause",
+              start: "center bottom",
+              end: "center top"
+            }
+          });
+
+          // add animations and labels to the timeline
+          tl_skills.from('.title-skills', {x: -300, y: -200, opacity: 0, duration: 1})
+            .from('.card-skills', {x: -300, rotation: 360, duration: 1}, "-=1")
+
+          let tl_contact = gsap.timeline({
+            // yes, we can add it to an entire timeline!
+            scrollTrigger: {
+              trigger: "#section-contact",
+              toggleActions: "restart pause resume pause",
+              start: "center bottom",
+              end: "center top"
+            }
+          })
+
+          // add animations and labels to the timeline
+          tl_contact.from('#image-contact', {x: -200, y: 500, opacity: 0, duration: 1.5})
+            .from('#form-contact', {y: 500, opacity: 0, duration: 1}, "-=1")
+        },
+        
+        // all 
+        "all": function() {
+          // ScrollTriggers created here aren't associated with a particular media query,
+          // so they persist.
+        }
+
+      }); 
     }
   }
 }
