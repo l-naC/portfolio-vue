ae97d142e43c71720842454cb0e899f5b84af7b6
Léna <lena.clavier@gmail.com>
2021-01-22 01:29:18 +0100

    Add animation scroll-snap part 2

--- src/components/ProjectsView.vue
+++ src/components/ProjectsView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-screen sm:h-auto md:h-screen lg:h-screen flex items-center justify-center flex-col py-5">
+    <div class="w-screen sm:h-auto md:h-screen lg:h-screen flex items-center justify-center flex-col py-5" id="section-projects">
         <h2 class="font-bold text-xl text-gray-600 m-3">Recent projects</h2>
         <div class="w-4/5 h-full flex flex-wrap justify-center flex-col sm:flex-col md:flex-row lg:flex-row m-5">
             <projects-card
